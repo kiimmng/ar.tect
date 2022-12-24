0ff9123f666ad1075f86fc9a0eac69c9b179dff5
kiimmng <121073116+kiimmng@users.noreply.github.com>
2022-12-25 00:36:12 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+$('#banner').owlCarousel({
+  loop: true,
+  items: 3,
+  smartSpeed: 4000,
+  autoplay: true,
+  autoplayTimeout: 9000,
+  nav: false,
+  mouseDrag: false,
+  dots: false,
+  startPosition: '#img-slide',
+})
+
+
---
+++ js/nav.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("nav").style.width = "180px";
+    document.getElementById("fullpage").style.marginLeft = "180px";
+}
+
+function closeNav() {
+    document.getElementById("nav").style.width = "0";
+    document.getElementById("fullpage").style.marginLeft= "0";
+}
